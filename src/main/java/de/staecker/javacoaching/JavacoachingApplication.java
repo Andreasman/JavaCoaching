315065e255219d86844585ca57dc8497438e1522
Andreas Staecker <andreas.staecker@t-online.de>
2017-09-12 16:13:52 +0200

    first

---
+++ src/main/java/de/staecker/javacoaching/JavacoachingApplication.java
@@ -0,0 +1,12 @@
+package de.staecker.javacoaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacoachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacoachingApplication.class, args);
+	}
+}
