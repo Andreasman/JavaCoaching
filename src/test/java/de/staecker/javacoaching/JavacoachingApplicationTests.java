406bd7bc4094537e37e219d1f58d5e5cfcb3535a
Andreas Staecker <andreas.staecker@t-online.de>
2017-09-12 16:14:23 +0200

    first commit

---
+++ src/test/java/de/staecker/javacoaching/JavacoachingApplicationTests.java
@@ -0,0 +1,16 @@
+package de.staecker.javacoaching;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavacoachingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
