6ce3344839a421ce4e2b37ba88da19ef64c6e854
DPP2SAD <andreas.staecker@volkswagen.de>
2022-12-08 09:34:23 +0100

    add Main Method

---
+++ src/main/java/de/staecker/javacoaching/MainDummy.java
@@ -0,0 +1,10 @@
+package de.staecker.javacoaching;
+
+import java.time.LocalDate;
+
+public class MainDummy {
+
+    public static void main(String[] args) {
+        System.out.println("hello World..." + LocalDate.now());
+    }
+}
